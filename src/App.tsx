ab2e49474e5527e63dfb793341fb89a25af4a2b5
gabiquintao <a27978@alunos.ipca.pt>
2025-07-26 21:38:46 +0100

    refactor: fix imports, paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Artists from './pages/Artists';
-import Songs from './pages/Songs';
+import Home from '@/pages/Home';
+import Artists from '@/pages/Artists';
+import Tracks from '@/pages/Tracks';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/artists" element={<Artists />} />
-      <Route path="/songs" element={<Songs />} />
+      <Route path="/tracks" element={<Tracks />} />
     </Routes>
   );
 }
